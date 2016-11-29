71e6bff722fe8a378b54b9f7d16e5d812d1221e3
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2016-11-29 14:57:30 +0100

    Initial Commit

---
+++ DocumentBasedApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DocumentBasedApplication
+//
+//  Created by Friedrich HAEUPL on 29.11.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
